633c69dc04e6ebe793eaad2c8cb4ffc881d8a3df
paul <sesshoumaru404@outlook.com>
2015-07-11 01:20:05 -0400

    counting all players

--- tournament/tournament.sql
+++ tournament/tournament.sql
@@ -8,7 +8,8 @@
 
 -- use in psql to \i tournament.sql of read file
 
--- DROP DATABASE IF EXISTS p2_tournament;
+SELECT pg_terminate_backend(procpid) FROM pg_stat_activity WHERE datname = 'p2_tournament';
+DROP DATABASE p2_tournament;
 CREATE DATABASE p2_tournament;
 
 \c p2_tournament;
@@ -23,13 +24,33 @@ CREATE TABLE players (
 CREATE TABLE matches (
     id1 integer REFERENCES players (id),
     id2 integer REFERENCES players (id),
-    match_id serial PRIMARY KEY,
-		UNIQUE   (id1, id2)
+    winner integer REFERENCES players (id),
+    match_id serial PRIMARY KEY
+		-- UNIQUE   (id1, id2)
 );
 
 CREATE VIEW standings AS (
-  SELECT ID, NAME
-  FROM PLAYERS
+  SELECT
+    players.id,
+    players.name,
+    count(matches.winner) as wins,
+    count(matches.match_id) as played
+  FROM PLAYERS LEFT JOIN MATCHES
+  ON players.id = matches.id1 OR players.id = matches.id2
+  -- order by id desc
+  GROUP BY players.id
+  -- HAVING id = matches.winner
 );
+DROP TABLE players;
+DROP TABLE matches;
+-- DROP VIEW standings;
+INSERT INTO players (name) VALUES ('test1');
+INSERT INTO players VALUES ('test2');
+INSERT INTO players VALUES ('test3');
+INSERT INTO matches VALUES (1,3,1);
+INSERT INTO matches VALUES (1,3,3);
+INSERT INTO matches VALUES (1,3,3);
 
+SELECT * from players;
+SELECT * from MATCHES;
 SELECT * from standings;
