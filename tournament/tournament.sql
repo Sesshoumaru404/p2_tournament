9c5dba2748a653415769fc88d65253455e321c40
Paul <sesshoumaru404@outlook.com>
2015-07-11 18:35:36 -0400

    standings & matches correct

--- tournament/tournament.sql
+++ tournament/tournament.sql
@@ -8,11 +8,14 @@
 
 -- use in psql to \i tournament.sql of read file
 
-SELECT pg_terminate_backend(procpid) FROM pg_stat_activity WHERE datname = 'p2_tournament';
-DROP DATABASE p2_tournament;
+-- SELECT pg_terminate_backend(procpid) FROM pg_stat_activity WHERE datname = 'p2_tournament';
+-- DROP DATABASE p2_tournament;
 CREATE DATABASE p2_tournament;
 
 \c p2_tournament;
+
+DROP TABLE IF EXISTS players CASCADE;
+DROP TABLE IF EXISTS matches CASCADE;
 
 CREATE TABLE players (
     name text,
@@ -29,11 +32,20 @@ CREATE TABLE matches (
 		-- UNIQUE   (id1, id2)
 );
 
-CREATE VIEW standings AS (
+CREATE VIEW win_lose AS (
   SELECT
     players.id,
-    players.name,
-    count(matches.winner) as wins,
+    count(matches.winner) as wins
+  FROM PLAYERS LEFT JOIN MATCHES
+  ON players.id = matches.winner
+  -- order by id desc
+  GROUP BY players.id
+  -- HAVING id = matches.winner
+);
+
+CREATE VIEW games AS (
+  SELECT
+    players.id,
     count(matches.match_id) as played
   FROM PLAYERS LEFT JOIN MATCHES
   ON players.id = matches.id1 OR players.id = matches.id2
@@ -41,16 +53,30 @@ CREATE VIEW standings AS (
   GROUP BY players.id
   -- HAVING id = matches.winner
 );
-DROP TABLE players;
-DROP TABLE matches;
+
+
+CREATE VIEW standings AS (
+  SELECT
+    players.id,
+    players.name,
+    win_lose.wins,
+    games.played
+  FROM PLAYERS INNER JOIN GAMES USING (id)
+  INNER JOIN WIN_LOSE USING (id)
+  ORDER BY wins DESC
+);
 -- DROP VIEW standings;
-INSERT INTO players (name) VALUES ('test1');
+INSERT INTO players (name) VALUES ('Paul');
 INSERT INTO players VALUES ('test2');
 INSERT INTO players VALUES ('test3');
-INSERT INTO matches VALUES (1,3,1);
-INSERT INTO matches VALUES (1,3,3);
-INSERT INTO matches VALUES (1,3,3);
+-- INSERT INTO matches VALUES (1,3,1);
+-- INSERT INTO matches VALUES (3,1,3);
+-- INSERT INTO matches VALUES (1,3,1);
+-- INSERT INTO matches VALUES (1,2,1);
+-- INSERT INTO matches VALUES (2,3,3);
 
 SELECT * from players;
 SELECT * from MATCHES;
+SELECT * from win_lose;
+SELECT * from games;
 SELECT * from standings;
